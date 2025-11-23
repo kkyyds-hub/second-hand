cc2e879704ce06fc3b3c19eda0d3c800d53af864
kk <kk@AE86.cn>
2025-11-23 17:03:31 +0800

    1

--- demo-service/src/main/java/com/demo/mapper/ProductMapper.java
+++ demo-service/src/main/java/com/demo/mapper/ProductMapper.java
@@ -1,11 +1,7 @@
 package com.demo.mapper;
 
-import com.demo.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 @Mapper
 public interface ProductMapper {
-    List<Product> getPendingApprovalProducts(String productName, String category, String status);
 }
